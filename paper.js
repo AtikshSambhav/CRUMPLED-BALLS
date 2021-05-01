36c9624224625dd2139c7d60892e40f239c0eb57
AtikshSambhav <78915859+AtikshSambhav@users.noreply.github.com>
2021-05-01 17:56:50 +0530

    Add files via upload
    SWACCHHA BHARAT ABHIYAAN Game.

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.0,
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y,radius-20/2 , options);
+      this.radius = radius;
+      this.image=loadImage("paper.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y)
+      imageMode(CENTER);
+      fill("yellow");
+      image(this.image,0, 0, this.radius, this.radius);
+      pop();
+    }
+  };
